75b70c0ea52ccd68385cc7252300f2a25f96ce74
Janna Ramirez Granados <a.22308051281287@cbtis128.edu.mx>
2025-02-11 16:53:52 +0000

    hola lista

---
+++ lista1.dart
@@ -0,0 +1,13 @@
+void main() {  
+  // LISTA TIPO ENTERO DE 3 ELEMENTOS
+   List<int> numeros = [10, 20, 30];
+   print("Lista de números enteros: ");
+  print(numeros);
+  print("Primer elemento: ${numeros[0]}");
+  print("Segundo elemento: ${numeros[1]}");
+  print("Tercer elemento: ${numeros[2]}");
+  print("Elementos de la lista usando ciclo for:");
+    for (int i = 0; i < numeros.length; i++) {
+    print(numeros[i]);
+  }
+}  
